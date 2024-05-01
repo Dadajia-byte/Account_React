1e7039739b5b2835928559512805daa6dab64b13
Dadajia <1814650097@qq.com>
2024-05-01 18:00:24 +0800

    TabBar实现

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,24 +1,62 @@
-import { Button } from "antd-mobile";
-import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { getBillList } from "@/store/modules/billStore";
+import { TabBar } from 'antd-mobile'
+import { Outlet, useNavigate } from 'react-router-dom'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+} from 'antd-mobile-icons'
+import './index.scss'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getBillList } from '@/store/modules/billStore'
 
+const tabs = [
+    {
+        key: '/',
+        title: '月度账单',
+        icon: <BillOutline />,
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />,
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />,
+    },
+]
 
 const Layout = () => {
-    const dispatch = useDispatch();
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getBillList())
     }, [dispatch])
-    return (<div>
-        <Outlet></Outlet>
-        我是layout
-        {/* 测试全局生效样式 */}
-        <Button color="primary">测试全局生效样式</Button>
-        <div className="puple">
-            <Button color="primary">测试局部</Button>
+
+    const switchRoute = (path) => {
+        navigate(path)
+    }
+
+    return (
+        <div className="layout">
+            <div className="container">
+                {/* 二级路由出口 */}
+                <Outlet />
+            </div>
+            <div className='footer'>
+                <TabBar
+                    onChange={switchRoute}
+                >
+                    {tabs.map(item => (
+                        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    ))}
+                </TabBar>
+            </div>
+
         </div>
-    </div>)
+    )
 }
 
-export default Layout;
+export default Layout
